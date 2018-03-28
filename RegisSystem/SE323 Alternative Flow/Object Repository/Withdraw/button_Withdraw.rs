9309790adcb8cd87c3ed9d9fa859191fca3660b3
Thanawat Lukuan <thanawat_lukuan@cmu.ac.th>
2018-03-28 12:19:00 +0700

    Update AlternativeFlow
    Edit
    -Withdraw
    -Enroll Course A2
    -Login Lecturer A2

--- RegisSystem/SE323 Alternative Flow/Object Repository/Withdraw/button_Withdraw.rs
+++ RegisSystem/SE323 Alternative Flow/Object Repository/Withdraw/button_Withdraw.rs
@@ -4,6 +4,12 @@
    <name>button_Withdraw</name>
    <tag></tag>
    <elementGuidId>a55bcec6-eab4-44f9-a8f1-5f4efb53f79d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;c7&quot;)/td[8]/button[@class=&quot;button is-danger&quot;][count(. | //button[(text() = 'Withdraw' or . = 'Withdraw')]) = count(//button[(text() = 'Withdraw' or . = 'Withdraw')])]</value>
+      </entry>
+   </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
@@ -28,7 +34,7 @@
       <value>Withdraw</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>false</isSelected>
+      <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
